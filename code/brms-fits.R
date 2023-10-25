d2a709db3b5dc72a46422757782332bd5f365707
sbh4th <sbh4th@gmail.com>
2023-10-25 07:33:22 -0400

    Start Bayesian imputation idea

--- code/brms-fits.R
+++ code/brms-fits.R
@@ -141,3 +141,58 @@ pd %>% select(ends_with("treat")) %>%
     color = cohort, fill = cohort)) + 
     scale_color_brewer(palette = "Set1") +
     stat_slab(.width = .95) + theme_bw()
+
+
+fe_model <- bf(y | subset(treat==0) ~ 1 + 
+  year_2012 + year_2013 + year_2014 + year_2015 +
+    year_2016 + year_2017 + year_2018 + year_2019 +
+    year_2020 + cohort_year_2014 + cohort_year_2016)
+w_model <- bf(w ~ 1 + e)
+
+b14.6 <-
+  brm(data = subdat_sim, 
+      family = gaussian,
+      e_model + w_model + set_rescor(TRUE),
+      prior = c(# E model
+                prior(normal(0, 0.2), class = Intercept, resp = e),
+                prior(normal(0, 0.5), class = b, resp = e),
+                prior(exponential(1), class = sigma, resp = e),
+                
+                # W model
+                prior(normal(0, 0.2), class = Intercept, resp = w),
+                prior(normal(0, 0.5), class = b, resp = w),
+                prior(exponential(1), class = sigma, resp = w),
+                
+                # rho
+                prior(lkj(2), class = rescor)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      seed = 14,
+      file = "fits/b14.06")
+
+
+b_first <-
+  brm(data = subset(db, treat==0), 
+      family = gaussian(),
+      y ~ 1 + (1 | state) + 
+        year_2012 + year_2013 + year_2014 + year_2015 +
+        year_2016 + year_2017 + year_2018 + year_2019 +
+        year_2020 + cohort_year_2014 + cohort_year_2016,
+      prior = c(prior(normal(0, 10), class = Intercept),
+        prior(normal(0, 5), class = b),
+        prior(exponential(1), class = sd)),
+      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      sample_prior = "yes", seed = 3958,
+      file = "code/fits/brms-re-fs")
+
+res <- residuals(b_first)
+
+newdata <- db
+
+library(tidybayes)
+
+db2 <- add_linpred_draws(object = b_first, 
+  newdata = db, draws=1)
+
+bpredict_values <- predict(b_first)
+
+db2 <- NULL
