a15b292de9a23b3e411c0acf47fb58816ab194eb
Sam Harper <sbh4th@gmail.com>
2024-03-01 18:29:31 -0500

    Update log outcome models

--- code/brms-fits.R
+++ code/brms-fits.R
@@ -170,23 +170,53 @@ b14.6 <-
       file = "fits/b14.06")
 
 
-b_first <-
-  brm(data = subset(db, treat==0), 
+b_second <-
+  brm(data = dsf, 
       family = gaussian(),
-      y ~ 1 + (1 | state) + 
-        year_2012 + year_2013 + year_2014 + year_2015 +
-        year_2016 + year_2017 + year_2018 + year_2019 +
-        year_2020 + cohort_year_2014 + cohort_year_2016,
-      prior = c(prior(normal(0, 10), class = Intercept),
+      y_it ~ 0 + (1 | state) + treat,
+      prior = c(
+        #prior(normal(0, 10), class = Intercept),
         prior(normal(0, 5), class = b),
         prior(exponential(1), class = sd)),
-      iter = 2000, warmup = 1000, chains = 4, cores = 4,
+      chains = 1, cores = 4,
       sample_prior = "yes", seed = 3958,
-      file = "code/fits/brms-re-fs")
+      file = "code/fits/brms-re-ss")
+
+bme_avg <- slopes(
+  b_second, 
+  newdata   = subset(db, treat==1),
+  variables = "treat", 
+  by        = "treat"
+)
+
+b_second_c_se <-
+  brm(data = dsf, 
+      family = gaussian(),
+      y_it | mi(se.pred) ~ 0 + (1 | state) + treat:cohort_year_2014 +
+        treat:cohort_year_2016,
+      prior = c(
+        #prior(normal(0, 10), class = Intercept),
+        prior(normal(0, 5), class = b),
+        prior(exponential(1), class = sd)),
+      chains = 1, cores = 4,
+      sample_prior = "yes", seed = 3958,
+      save_pars = save_pars(latent = TRUE),
+      file = "code/fits/brms-re-ss_c_se")
+
+b_second_c_me <- slopes(
+  b_second_c, 
+  newdata   = subset(db, treat & cohort_year),
+  variables = "treat", 
+  by        = "cohort_year"
+)
 
 res <- residuals(b_first)
 
 newdata <- db
+
+year_2012 + year_2013 + year_2014 + year_2015 +
+  year_2016 + year_2017 + year_2018 + year_2019 +
+  year_2020 + cohort_year_2014 + cohort_year_2016
 
 library(tidybayes)
 
@@ -195,4 +225,24 @@ db2 <- add_linpred_draws(object = b_first,
 
 bpredict_values <- predict(b_first)
 
-db2 <- NULL
+
+dsf2 <- d %>% 
+  select(y,state,year,treat,cohort_year) %>% 
+  add_predictions(fs, se.fit = TRUE) 
+%>%
+  # add_predictions(fs, var = "pred_se", se.fit=TRUE) %>%
+  mutate(y_it = y - pred,
+         event = year - cohort_year) 
+
+
+b_second_se <-
+  brm(data = dsf, 
+      family = gaussian(),
+      y_it | mi(se.pred) ~ 0 + (1 | state) + treat,
+      prior = c(
+        #prior(normal(0, 10), class = Intercept),
+        prior(normal(0, 5), class = b),
+        prior(exponential(1), class = sd)),
+      chains = 4, cores = 4,
+      sample_prior = "yes", seed = 3958,
+      file = "code/fits/brms-re-ss_se")
