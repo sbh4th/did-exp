c8d8af18cdc1d30194b17b363496027b589235d6
Sam Harper <sbh4th@gmail.com>
2025-01-15 20:21:06 +0100

    Update code for ITS simulation

--- code/its-sim.R
+++ code/its-sim.R
@@ -43,11 +43,14 @@ dtest <- expand_grid(unit = 1:1000, year = 2001:2020) %>%
          treat = ifelse((year >= 
            cohort_year), 1, 0),
          # treatment effect = 3 if 2015, 6 if 2010, annually
-         mu = ifelse(cohort_year==2015, 4, 1),
+         mu = ifelse(cohort_year==2015, -2, 2),
          tau = ifelse(treat == 1, mu, 0),
          # year trends differ by cohort
-         year_fe = year_fe + (2020 - cohort_year) * 
-           (year - cohort_year) / 5 + 20
+         year_fe = ifelse(cohort_year==2015 & year<2005,
+          year_fe + (2015 - cohort_year) * 
+           (year - cohort_year) / 5 + 12,
+          year_fe + (2020 - cohort_year) *
+           (year - cohort_year) / 5 + 20)
   ) %>% 
   # calculate cumulative treatment effects
   group_by(unit) %>% 
@@ -60,30 +63,37 @@ dtest <- expand_grid(unit = 1:1000, year = 2001:2020) %>%
   # mutate(cohort_year = ifelse(cohort_year == 2018, Inf, cohort_year))
 
 # bring in simulated DiD dataset
-ds <- dtest %>%
+dss <- dtest %>%
   # read_rds(here("data", 
  #"did-sim-data.rds")) %>%
   mutate(year0 = year - 2001,
-         yearc = year - cohort_year,
-         yearct = yearc * treat)
+    yearc = year - cohort_year,
+    cohort_2015 = ifelse(cohort_year == 2015, 1, 0))
 
 # plots
-ds %>% 
+dss %>% 
   ggplot(aes(x = year0, y = y, group = unit)) + 
   geom_line(alpha = 1/8, color = "grey") + 
-  geom_line(data = ds %>% 
+  geom_line(data = dss %>% 
     group_by(cohort_year, year0) %>% 
     summarize(y = mean(y)),
    aes(x = year0, y = y, group = factor(cohort_year),
     color = factor(cohort_year)), linewidth = 2) + 
+  geom_line(data = dss %>%
+    group_by(year0) %>%
+      summarize(y = mean(y)),
+    aes(x = year0, y = y, color = "#4daf4a", group="All"), 
+    linewidth = 1, linetype = 2) +
   labs(x = "", y = "Y",  color = "Treatment group   ") +
   scale_x_continuous(breaks = c(5, 10, 15, 20)) +
   geom_vline(xintercept = 10, color = '#E41A1C', linewidth = 2) + 
   geom_vline(xintercept = 15, color = '#377EB8', linewidth = 2) + 
-  scale_color_brewer(palette = 'Set1') 
+  scale_color_brewer(palette = 'Set1', 
+    labels = c("2010", "2015", "Average")) + 
+  theme_classic()
 
 # plot of average (ignoring cohorts)
-ds %>% 
+dss %>% 
   ggplot(aes(x = year0, y = y)) + 
   geom_line(aes(group=unit), alpha = 1/8, color = "grey") +
   geom_line(data = ds %>% 
@@ -92,6 +102,62 @@ ds %>%
    aes(x = year0, y = y), linewidth=2) + 
   labs(x = "", y = "Y") +
   scale_x_continuous(breaks = c(5, 10, 15, 20)) 
+
+# relative time
+dss %>% 
+  ggplot(aes(x = yearc, y = y, group = unit)) + 
+  geom_line(alpha = 1/8, color = "grey") + 
+  geom_line(data = dss %>% 
+    group_by(cohort_year, yearc) %>% 
+    summarize(y = mean(y)),
+   aes(x = yearc, y = y, group = factor(cohort_year),
+    color = factor(cohort_year)), linewidth = 2) + 
+  labs(x = "", y = "Y",  color = "Treatment group   ") +
+  scale_x_continuous(breaks = c(-5, 0, 5, 10)) +
+  geom_vline(xintercept = 0, color = '#E41A1C', linewidth = 2) + 
+  geom_vline(xintercept = 0, color = '#377EB8', linewidth = 2) + 
+  scale_color_brewer(palette = 'Set1') 
+
+# relative time
+dss %>% 
+  ggplot(aes(x = yearc, y = y, group = unit)) + 
+  geom_line(alpha = 1/8, color = "grey") + 
+  geom_line(data = dss %>% 
+    group_by(cohort_year, yearc) %>% 
+    summarize(y = mean(y)),
+   aes(x = yearc, y = y, group = factor(cohort_year),
+    color = factor(cohort_year)), linewidth = 2) +
+  geom_line(data = dss %>%
+    group_by(yearc) %>%
+      summarize(y = mean(y)),
+    aes(x = yearc, y = y, color = "#4daf4a", group="All"), 
+    linewidth = 2) +
+  labs(x = "", y = "Y",  color = "Treatment group   ") +
+  scale_x_continuous(breaks = c(-5, 0, 5, 10)) +
+  geom_vline(xintercept = 0, color = '#984ea3', linewidth = 2) + 
+  scale_color_brewer(palette = 'Set1', labels = c("2010", "2015", "Average")) + theme_classic()
+
+# relative time
+dss %>%
+  filter(yearc >= -9 & yearc <= 5) %>%
+  ggplot(aes(x = yearc, y = y, group = unit)) + 
+  geom_line(alpha = 1/8, color = "grey") + 
+  geom_line(data = dss %>% 
+    filter(yearc >= -9 & yearc <= 5) %>%
+    group_by(cohort_year, yearc) %>% 
+    summarize(y = mean(y)),
+   aes(x = yearc, y = y, group = factor(cohort_year),
+    color = factor(cohort_year)), linewidth = 2) +
+  geom_line(data = dss %>%
+    filter(yearc >= -9 & yearc <= 5) %>%
+    group_by(yearc) %>%
+      summarize(y = mean(y)),
+    aes(x = yearc, y = y, color = "#4daf4a", group="All"), 
+    linewidth = 2) +
+  labs(x = "", y = "Y",  color = "Treatment group   ") +
+  scale_x_continuous(breaks = c(-5, 0, 5, 10)) +
+  geom_vline(xintercept = 0, color = '#984ea3', linewidth = 2) + 
+  scale_color_brewer(palette = 'Set1', labels = c("2010", "2015", "Average")) + theme_classic()
 
 m1 <- lm_robust(y ~ year, data=dtest, clusters = state)
 tidy(m1)
@@ -134,10 +200,61 @@ mitsc_yc <- lm_robust(y ~ year0 + treat +
 mits_yc_r <- lm_robust(y ~ yearc + treat + 
   yearct, data = subset(ds, yearc>=-9 & yearc<=5), 
   clusters = state)
-mitsc_yc_r <- lm_robust(y ~ year0 + treat + 
+mitsc_yc_r <- lm_robust(y ~ yearc + treat + 
   yearct + factor(cohort_year), 
   data = subset(ds, yearc>=-9 & yearc<=5),
   clusters = state)
 
 modelsummary(list(mits,mitsc))
+
+
+ds %>% 
+  filter(cohort_2015==1) %>%
+  ggplot(aes(x = year0, y = y, group = unit)) + 
+  geom_line(alpha = 1/8, color = "grey") + 
+  geom_smooth(data = ds %>%
+    filter(cohort_2015==1) %>%
+    group_by(year0, treat) %>%
+      summarize(y = mean(y)),
+    aes(x = year0, y = y, color = factor(treat), 
+        group = factor(treat)), 
+    linewidth = 1, method='lm') +
+  geom_smooth(data = ds %>%
+    filter(cohort_2015==1) %>%
+    group_by(year0, treat) %>%
+      summarize(y = mean(y)),
+    aes(x = year0, y = y, color = factor(treat), 
+        group = factor(treat)), 
+    linewidth = 1, method='lm') +
+  labs(x = "", y = "Y",  color = "Treatment group   ") +
+  scale_x_continuous(breaks = c(5, 10, 15, 20)) +
+  geom_vline(xintercept = 15, color = '#377EB8', linewidth = 2) + 
+  scale_color_brewer(palette = 'Set1', 
+    labels = c("Pre", "Post")) + 
+  theme_classic()
+
+dss %>% 
+  filter(cohort_2015==1) %>%
+  ggplot(aes(x = year0, y = y, group = unit)) + 
+  geom_line(alpha = 1/8, color = "grey") + 
+  geom_smooth(data = dss %>%
+    filter(cohort_2015==1) %>%
+    group_by(year0, treat) %>%
+      summarize(y = mean(y)),
+    aes(x = year0, y = y, color = factor(treat), 
+        group = factor(treat)), 
+    linewidth = 1, method='lm') +
+  geom_smooth(data = dss %>%
+    filter(cohort_2015==1) %>%
+    group_by(year0, treat) %>%
+      summarize(y = mean(y)),
+    aes(x = year0, y = y, color = factor(treat), 
+        group = factor(treat)), 
+    linewidth = 1, method='lm') +
+  labs(x = "", y = "Y",  color = "Treatment group   ") +
+  scale_x_continuous(breaks = c(5, 10, 15, 20)) +
+  geom_vline(xintercept = 15, color = '#377EB8', linewidth = 2) + 
+  scale_color_brewer(palette = 'Set1', 
+    labels = c("Pre", "Post")) + 
+  theme_classic()
 
